e66e5c18bc7715a6d46ebbbcb759c33d8fa814a2
Daniil Razdymakha <daniilrazdymaha@gmail.com>
2025-06-25 14:53:11 +0700

    Feat: add method shakerSort(array)

--- src/io/iprody/lessons/lesson6/HomeWork.java
+++ src/io/iprody/lessons/lesson6/HomeWork.java
@@ -13,6 +13,9 @@ public class HomeWork {
         int[] sourceArray1 = {1, 2, 3, 4};
         int[] target1 = {5, 6, 7};
         target1 = arrayCopy(sourceArray1, target1);
+
+        System.out.println();
+        shakerSort(target);
     }
 
     public static int[] arrayCopy(int[] source, int[] target) {
@@ -51,4 +54,39 @@ public class HomeWork {
         return copyTwoArrays;
     }
 
+    public static void shakerSort(int[] array) {
+        System.out.print("Массив до шейкерной сортировки - ");
+        System.out.println(Arrays.toString(array));
+
+        int leftSideArray = 0;
+        int rightSideArray = array.length - 1;
+        boolean isSorted = false;
+
+        while (!isSorted && (leftSideArray < rightSideArray)) {
+            isSorted = true; // Предполагаем, что массив изначально отсортирован
+
+            for (int i = leftSideArray; i < rightSideArray; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            rightSideArray--;
+
+            for (int i = rightSideArray; i > leftSideArray; i--) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            leftSideArray++;
+        }
+
+        System.out.print("Массив после шейкерной сортировки - ");
+        System.out.println(Arrays.toString(array));
+    }
 }
